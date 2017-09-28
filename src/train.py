1359f7d84a7a50d4151e4a19bb972032dacb084c
Evgeny Toropov <toropov.evgeny@gmail.com>
2017-09-28 15:22:32 -0400

    Wrote quadro

--- src/train.py
+++ src/train.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-
 """
 Copyright (C) 2017 NVIDIA Corporation.  All rights reserved.
 Licensed under the CC BY-NC-ND 4.0 license (https://creativecommons.org/licenses/by-nc-nd/4.0/legalcode).
@@ -71,6 +69,10 @@ def make_save_image(images_a, x_aa, x_ab, images_b, x_ba, x_bb):
   assembled_images = torch.cat((images_a, x_aa, x_ab, images_b, x_ba, x_bb), 3)
   return assembled_images
 
+def make_save_image_any(images_list):
+  assembled_images = torch.cat(tuple([standardize_image(x) for x in images_list]), 3)
+  return assembled_images
+
 def get_model_list(dirname, key):
   if os.path.exists(dirname) is False:
     return None
@@ -83,7 +85,7 @@ def get_model_list(dirname, key):
   return last_model_name
 
 def resume(trainer, snapshot_prefix):
-  print('Loading G and D models... ', end='')
+  print('Loading G and D models... ')
   start = time.time()
   dirname = os.path.dirname(snapshot_prefix)
   last_model_name = get_model_list(dirname,"gen")
@@ -96,6 +98,26 @@ def resume(trainer, snapshot_prefix):
   print('loaded in %.1f sec.' % (time.time()-start))
   print('Resume from iteration %d' % iterations)
   return iterations
+
+def resume_quadro(trainer, snapshot_prefix_b, snapshot_prefix_c):
+  def _resume(trainer, snapshot_prefix, index):
+    start = time.time()
+    dirname = os.path.dirname(snapshot_prefix)
+    last_model_name = get_model_list(dirname,"gen")
+    if last_model_name is None:
+        print ('Did not find model %s' % last_model_name)
+        return 0
+    print('Loading G and D models from %s for index %d... ' % (last_model_name, index))
+    trainer.gen[index].load_state_dict(torch.load(last_model_name))
+    iterations = int(last_model_name[-12:-4])
+    last_model_name = get_model_list(dirname, "dis")
+    trainer.dis[index].load_state_dict(torch.load(last_model_name))
+    print('loaded in %.1f sec.' % (time.time()-start))
+    print('Resume from iteration %d' % iterations)
+    return iterations
+  iterations_b = _resume(trainer, snapshot_prefix_b, 0)
+  iterations_c = _resume(trainer, snapshot_prefix_c, 1)
+  return max(iterations_b, iterations_c)
 
 def get_data_loader(conf, batch_size, context=0):
   data_a = []
@@ -142,12 +164,10 @@ def write_html(filename, iterations, image_save_iterations, image_directory, ima
   html_file.close()
 
 
-def main(argv):
-  (opts, args) = parser.parse_args(argv)
-  # Load experiment setting
-  assert isinstance(opts, object)
+def unit(opts):
   config = NetConfig(opts.config)
-  train_writer = tensorboard.FileWriter("%s/%s" % (opts.log,os.path.splitext(os.path.basename(opts.config))[0]))
+  train_writer = tensorboard.FileWriter(
+      "%s/%s" % (opts.log,os.path.splitext(os.path.basename(opts.config))[0]))
 
   max_iterations = config.hyperparameters['max_iterations']
   batch_size = config.hyperparameters['batch_size']
@@ -164,8 +184,8 @@ def main(argv):
   input_dims.append(config.datasets['b']['channels'])
 
   # Load datasets
-  train_loader_a = get_data_loader(config.datasets['a'], batch_size)
-  train_loader_b = get_data_loader(config.datasets['b'], batch_size)
+  train_loader_a1 = get_data_loader(config.datasets['a'], batch_size)
+  train_loader_b1 = get_data_loader(config.datasets['b'], batch_size)
   train_loader_a2 = get_data_loader(config.datasets['a'], batch_size)
   train_loader_b2 = get_data_loader(config.datasets['b'], batch_size)
   trainer = UNITTrainer(gen_net, dis_net, batch_size, ch, input_dims, image_size, opts.lr)
@@ -183,19 +203,21 @@ def main(argv):
   if not os.path.exists(image_directory):
     os.makedirs(image_directory)
 
-  write_html(directory + "/index.html", iterations + 1, config.image_save_iterations, image_directory, image_size)
+  write_html(directory + "/index.html", iterations + 1,
+             config.image_save_iterations, image_directory, image_size)
 
   for ep in range(0, MAX_EPOCHS):
-    for it, (images_a, images_b, images_a2, images_b2) in enumerate(itertools.izip(train_loader_a, train_loader_b, train_loader_a2, train_loader_b2)):
-      if images_a.size(0) != batch_size or images_b.size(0) != batch_size:
+    for it, (images_a1, images_b1, images_a2, images_b2) in enumerate(
+         itertools.izip(train_loader_a1, train_loader_b1, train_loader_a2, train_loader_b2)):
+      if images_a1.size(0) != batch_size or images_b1.size(0) != batch_size:
         continue
-      images_a = Variable(images_a.cuda(opts.gpu))
-      images_b = Variable(images_b.cuda(opts.gpu))
+      images_a1 = Variable(images_a1.cuda(opts.gpu))
+      images_b1 = Variable(images_b1.cuda(opts.gpu))
       images_a2 = Variable(images_a2.cuda(opts.gpu))
       images_b2 = Variable(images_b2.cuda(opts.gpu))
       # Main training code
-      trainer.dis_update(images_a, images_b, images_a2, images_b2)
-      x_aa, x_ba, x_ab, x_bb = trainer.gen_update(images_a, images_b, gan_w, vae_ll_w, vae_enc_w)
+      trainer.dis_update(images_a1, images_b1, images_a2, images_b2)
+      x_aa, x_ba, x_ab, x_bb = trainer.both_gen_update(images_a1, images_b1, gan_w, vae_ll_w, vae_enc_w)
 
       # Dump training stats in log file
       if (iterations+1) % config.display == 0:
@@ -212,12 +234,12 @@ def main(argv):
 
       # Save intermediate visualization results
       if (iterations+1) % config.image_save_iterations == 0:
-        assembled_images = make_save_image(images_a[0:1,::], x_aa[0:1,::], x_ab[0:1,::], images_b[0:1,::], x_ba[0:1,::], x_bb[0:1,::])
+        assembled_images = make_save_image(images_a1[0:1,::], x_aa[0:1,::], x_ab[0:1,::], images_b1[0:1,::], x_ba[0:1,::], x_bb[0:1,::])
         img_filename = '%s/gen_%08d.jpg' % (image_directory, iterations + 1)
         torchvision.utils.save_image(assembled_images.data / 2 + 0.5, img_filename, nrow=1)
         write_html(directory + "/index.html", iterations + 1, config.image_save_iterations, image_directory, image_size)
       else:
-        assembled_images = make_save_image(images_a[0:1,::], x_aa[0:1,::], x_ab[0:1,::], images_b[0:1,::],x_ba[0:1,::], x_bb[0:1,::])
+        assembled_images = make_save_image(images_a1[0:1,::], x_aa[0:1,::], x_ab[0:1,::], images_b[0:1,::],x_ba[0:1,::], x_bb[0:1,::])
         img_filename = '%s/gen.jpg' % (image_directory)
         torchvision.utils.save_image(assembled_images.data / 2 + 0.5, img_filename, nrow=1)
 
@@ -228,12 +250,144 @@ def main(argv):
         torch.save(trainer.gen.state_dict(), gen_filename)
         torch.save(trainer.dis.state_dict(), dis_filename)
 
+      iterations += 1
+      if iterations == max_iterations:
+        return
 
 
+def quadro(opts):
+  config = NetConfig(opts.config)
+  train_writer = tensorboard.FileWriter(
+      "%s/%s" % (opts.log,os.path.splitext(os.path.basename(opts.config))[0]))
+
+  max_iterations = config.hyperparameters['max_iterations']
+  batch_size = config.hyperparameters['batch_size']
+  vae_enc_w = config.hyperparameters['vae_enc_w']
+  vae_ll_w = config.hyperparameters['vae_ll_w']
+  gan_w = config.hyperparameters['gan_w']
+  quadro_w = config.hyperparameters['quadro_w']
+  ch = config.hyperparameters['ch']
+  gen_net = config.hyperparameters['gen']
+  dis_net = config.hyperparameters['dis']
+
+  image_size = config.datasets['a']['image_size']
+  input_dims = list()
+  input_dims.append(config.datasets['a']['channels'])
+  input_dims.append(config.datasets['b']['channels'])
+
+  # Load datasets
+  train_loader_a1 = get_data_loader(config.datasets['a'], batch_size)
+  train_loader_b1 = get_data_loader(config.datasets['b'], batch_size)
+  train_loader_c1 = get_data_loader(config.datasets['c'], batch_size)
+  train_loader_a2 = get_data_loader(config.datasets['a'], batch_size)
+  train_loader_b2 = get_data_loader(config.datasets['b'], batch_size)
+  train_loader_c2 = get_data_loader(config.datasets['c'], batch_size)
+  trainer = UNITQuadroTrainer(gen_net, dis_net, batch_size, ch, input_dims, image_size, opts.lr)
+
+  iterations = 0
+  if opts.resume == 1:
+    iterations = resume_quadro(trainer, config.snapshot_prefix_b, config.snapshot_prefix_c)
+
+  trainer.cuda(opts.gpu)
+
+  directory = os.path.dirname(config.snapshot_prefix)
+  image_directory = directory + "/images"
+  if not os.path.exists(directory):
+    os.makedirs(directory)
+  if not os.path.exists(image_directory):
+    os.makedirs(image_directory)
+
+  write_html(directory + "/index.html", iterations + 1,
+      config.image_save_iterations, image_directory, image_size)
+
+  start = time.time()
+  for ep in range(0, MAX_EPOCHS):
+    for it, (images_a1, images_b1, images_c1,
+             images_a2, images_b2, images_c2) in enumerate(itertools.izip(
+               train_loader_a1, train_loader_b1, train_loader_c1,
+               train_loader_a2, train_loader_b2, train_loader_c2)):
+      if (images_a1.size(0) != batch_size or images_b1.size(0) != batch_size or
+          images_c1.size(0) != batch_size):
+        continue
+      images_a1 = Variable(images_a1.cuda(opts.gpu))
+      images_b1 = Variable(images_b1.cuda(opts.gpu))
+      images_c1 = Variable(images_c1.cuda(opts.gpu))
+      images_a2 = Variable(images_a2.cuda(opts.gpu))
+      images_b2 = Variable(images_b2.cuda(opts.gpu))
+      images_c2 = Variable(images_c2.cuda(opts.gpu))
+
+      # Test trained models before training.
+      if ep == 0 and it == 0:
+        img_filename = '%s/try_a.jpg' % image_directory
+        torchvision.utils.save_image(images_a1.data / 2 + 0.5, img_filename, nrow=1)
+        x_ab, x_ac = trainer.translate(images_a1)
+        img_filename = '%s/try_ab.jpg' % image_directory
+        torchvision.utils.save_image(x_ab.data / 2 + 0.5, img_filename, nrow=1)
+        img_filename = '%s/try_ac.jpg' % image_directory
+        torchvision.utils.save_image(x_ac.data / 2 + 0.5, img_filename, nrow=1)
+
+      # Main training code
+      trainer.both_dis_update(images_a1, images_b1, images_c1, images_a2, images_b2, images_c2)
+      x_ab, x_ac, x_bd, x_cd = trainer.both_gen_update(images_a1, images_b1, images_c1,
+          gan_w, vae_ll_w, vae_enc_w, quadro_w)
+
+      # Dump training stats in log file
+      if (iterations+1) % config.display == 0:
+        sec_per_iter = (time.time() - start) / config.display
+        print("Iteration: %08d/%08d, sec/iteration: %f" %
+            (iterations+1, max_iterations, sec_per_iter))
+        start = time.time()
+        members = [attr for attr in dir(trainer) if
+                   not callable(getattr(trainer, attr)) and
+                   not attr.startswith("__") and
+                   ('loss' in attr or 'acc' in attr)]
+        for m in members:
+          val = getattr(trainer, m)
+          if type(val) is list:
+            # Make a name for each element of the list, and record the value.
+            for i,el in enumerate(val):
+              name = m + '_%d' % i
+              train_writer.add_summary(summary.scalar(name, el), iterations + 1)
+          else:
+            train_writer.add_summary(summary.scalar(m, val), iterations + 1)
+
+      # Save intermediate visualization results
+      if (iterations+1) % config.image_save_iterations == 0:
+        assembled_images = make_save_image_any([
+            images_a1, images_b1, images_c1, x_ab, x_ac, x_bd, x_cd])
+        img_filename = '%s/gen_%08d.jpg' % (image_directory, iterations + 1)
+        torchvision.utils.save_image(assembled_images.data / 2 + 0.5, img_filename, nrow=1)
+        write_html(directory + "/index.html", iterations + 1,
+            config.image_save_iterations, image_directory, image_size)
+        #train_writer.add_summary(summary.image('images', assembled_images), iterations + 1)
+      else:
+        assembled_images = make_save_image_any([
+            images_a1, images_b1, images_c1, x_ab, x_ac, x_bd, x_cd])
+        img_filename = '%s/gen.jpg' % (image_directory)
+        torchvision.utils.save_image(assembled_images.data / 2 + 0.5, img_filename, nrow=1)
+
+      # Save network weights
+      if (iterations+1) % config.snapshot_save_iterations == 0:
+        gen_filename = '%s_b_gen_%08d.pkl' % (config.snapshot_prefix, iterations + 1)
+        dis_filename = '%s_b_dis_%08d.pkl' % (config.snapshot_prefix, iterations + 1)
+        torch.save(trainer.gen[0].state_dict(), gen_filename)
+        torch.save(trainer.dis[0].state_dict(), dis_filename)
+        gen_filename = '%s_c_gen_%08d.pkl' % (config.snapshot_prefix, iterations + 1)
+        dis_filename = '%s_c_dis_%08d.pkl' % (config.snapshot_prefix, iterations + 1)
+        torch.save(trainer.gen[1].state_dict(), gen_filename)
+        torch.save(trainer.dis[1].state_dict(), dis_filename)
 
       iterations += 1
       if iterations == max_iterations:
         return
+
+
+def main(argv):
+  (opts, args) = parser.parse_args(argv)
+  # Load experiment setting
+  assert isinstance(opts, object)
+  quadro(opts)
+  # unit(opts)
 
 if __name__ == '__main__':
   main(sys.argv)
--- src/trainers/unit_trainer.py
+++ src/trainers/unit_trainer.py
@@ -2,6 +2,8 @@
 Copyright (C) 2017 NVIDIA Corporation.  All rights reserved.
 Licensed under the CC BY-NC-ND 4.0 license (https://creativecommons.org/licenses/by-nc-nd/4.0/legalcode).
 """
+from __future__ import print_function
+
 import time
 from unit_nets import *
 from init import *
@@ -424,4 +426,169 @@ class UNITTrainer(nn.Module):
     self.dis_true_acc = 0.5 * (true_a_acc + true_b_acc)
     self.dis_fake_acc = 0.5 * (fake_a_acc + fake_b_acc)
     return
+
+
+class UNITQuadroTrainer(nn.Module):
+  def _compute_ll_loss(self,a,b):
+    return self.ll_loss_criterion(a, b) * b.size(1) * b.size(2) * b.size(3)
+
+  def _compute_kl(self, mu, sd):
+    mu_2 = torch.pow(mu, 2)
+    sd_2 = torch.pow(sd, 2)
+    encoding_loss = (mu_2 + sd_2 - torch.log(sd_2)).sum() / mu_2.size(0)
+    return encoding_loss
+
+  def _compute_true_acc(self,predictions):
+    predictions = torch.ge(predictions.data, 0.5)
+    if len(predictions.size()) == 3:
+      predictions = predictions.view(predictions.size(0) * predictions.size(1) * predictions.size(2))
+    acc = (predictions == 1).sum() / (1.0 * predictions.size(0))
+    return acc
+
+  def _compute_fake_acc(self,predictions):
+    predictions = torch.le(predictions.data, 0.5)
+    if len(predictions.size()) == 3:
+      predictions = predictions.view(predictions.size(0) * predictions.size(1) * predictions.size(2))
+    acc = (predictions == 1).sum() / (1.0 * predictions.size(0))
+    return acc
+
+  def cuda(self, gpu):
+    self.gpu = gpu
+    self.dis[0].cuda(self.gpu)
+    self.gen[0].cuda(self.gpu)
+    self.dis[1].cuda(self.gpu)
+    self.gen[1].cuda(self.gpu)
+    self.dis_labels = self.dis_labels.cuda(self.gpu)
+    self.gen_labels = self.gen_labels.cuda(self.gpu)
+
+  def __init__(self, gen, dis, batch_size, ch, input_dims, image_size, lr):
+    '''
+    Args:
+      gen:  a string with the type of generator, e.g. 'CoVAE'.
+      dis:  a string with the type of discriminator, e.g. 'CoDis'.
+      batch_size:  e.g., 1.
+      ch:   base width of both generator and discriminator, e.g. 32.
+      input_dims:  number of image channels in a and b domains, e.g. [4, 4].
+      image_size:  ???, e.g. 256.
+      lr:   e.g. 0.0001.
+    '''
+    super(UNITQuadroTrainer, self).__init__()
+    assert dis == 'CoDis'
+    self.dis = [CoDis(ch, input_dims),
+                CoDis(ch, input_dims)]
+    assert gen == 'CoVAE'
+    self.gen = [CoVAE(ch, input_dims, input_dims, image_size),
+                CoVAE(ch, input_dims, input_dims, image_size)]
+    self.dis_opt = torch.optim.Adam(self.dis[0].parameters(), lr=lr,
+                                    betas=(0.5, 0.999), weight_decay=0.0001)
+    self.gen_opt = torch.optim.Adam(self.gen[0].parameters(), lr=lr,
+                                    betas=(0.5, 0.999), weight_decay=0.0001)
+    n_per_side = image_size/self.dis[0].input_size
+    if n_per_side < 1:
+      ones_labels = Variable(torch.ones((batch_size * 2)))
+      zeros_labels = Variable(torch.zeros((batch_size * 2)))
+    else:
+      ones_labels = Variable(torch.ones((batch_size*2,n_per_side,n_per_side)))
+      zeros_labels = Variable(torch.zeros((batch_size*2,n_per_side,n_per_side)))
+    self.dis_labels = torch.cat((ones_labels, zeros_labels, ones_labels, zeros_labels), 0)
+    self.gen_labels = torch.cat((ones_labels, ones_labels), 0)
+    self.dis[0].apply(gaussian_weights_init)
+    self.dis[1].apply(gaussian_weights_init)
+    self.gen[0].apply(gaussian_weights_init)
+    self.gen[1].apply(gaussian_weights_init)
+    self.ll_loss_criterion = torch.nn.L1Loss()
+    self.tv_loss_criterion = torch.nn.L1Loss()
+    self.gpu = 0
+    # Lists for summaries from the two generators and detectors.
+    self.dis_loss = [0, 0]
+    self.dis_true_acc = [0, 0]
+    self.dis_fake_acc = [0, 0]
+    self.gen_ad_loss = [0, 0]
+    self.gen_ll_loss_a = [0, 0]
+    self.gen_ll_loss_b = [0, 0]
+    self.gen_encoding_loss = [0, 0]
+
+  def translate(self, x):
+    # Test loaded generators.
+    x_ab, _ = self.gen[0].translate_a_to_b(x, self.gpu)
+    x_ac, _ = self.gen[1].translate_a_to_b(x, self.gpu)
+    return x_ab, x_ac
+
+  def gen_update(self, index, images_a, images_b, gan_w, vae_ll_w, vae_enc_w):
+    '''
+    Args:
+      index:  0 or 1, one of the two generators
+    '''
+    self.gen[index].zero_grad()
+    x_aa, x_ba, x_ab, x_bb, lt_codes = self.gen[index](images_a, images_b, self.gpu)
+    data_a = torch.cat((x_aa, x_ba), 0)
+    data_b = torch.cat((x_bb, x_ab), 0)
+    outputs = nn.functional.sigmoid(self.dis[index](data_a, data_b))
+    ad_loss = nn.functional.binary_cross_entropy(outputs, self.gen_labels)
+    ll_loss_a = self._compute_ll_loss(x_aa, images_a)
+    ll_loss_b = self._compute_ll_loss(x_bb, images_b)
+    encoding_loss = 0
+    for i, lt in enumerate(lt_codes):
+      encoding_loss += self._compute_kl(*lt)
+    total_loss = gan_w * ad_loss + vae_ll_w * (ll_loss_a + ll_loss_b) + vae_enc_w * encoding_loss
+#    total_loss.backward()
+#    self.gen_opt.step()
+    self.gen_ad_loss[index] = ad_loss.data.cpu().numpy()[0]
+    self.gen_ll_loss_a[index] = ll_loss_a.data.cpu().numpy()[0]
+    self.gen_ll_loss_b[index] = ll_loss_b.data.cpu().numpy()[0]
+    self.gen_encoding_loss[index] = encoding_loss.data.cpu().numpy()[0]
+    return x_aa, x_ba, x_ab, x_bb, total_loss
+
+  def dis_update(self, index, images_a1, images_b1, images_a2, images_b2):
+    '''
+    Args:
+      index:  0 or 1, one of the two generators
+    '''
+    self.dis[index].zero_grad()
+    x_aa, x_ba, x_ab, x_bb, lt_codes = self.gen[index](images_a2, images_b2, self.gpu)
+    data_a = torch.cat((images_a1, images_a2, x_aa, x_ba), 0)
+    data_b = torch.cat((images_b1, images_b2, x_bb, x_ab), 0)
+    outputs = nn.functional.sigmoid(self.dis[0](data_a, data_b))
+    loss = nn.functional.binary_cross_entropy(outputs, self.dis_labels)
+    #loss.backward()
+    #self.dis_opt.step()
+    outputs_a, outputs_b = torch.split(outputs, data_a.size(0), dim=0)
+    outputs_a_true, outputs_a_fake = torch.split(outputs_a, data_a.size(0) // 2, dim=0)
+    outputs_b_true, outputs_b_fake = torch.split(outputs_b, data_b.size(0) // 2, dim=0)
+    true_a_acc = _compute_true_acc(outputs_a_true)
+    true_b_acc = _compute_true_acc(outputs_b_true)
+    fake_a_acc = _compute_fake_acc(outputs_a_fake)
+    fake_b_acc = _compute_fake_acc(outputs_b_fake)
+    self.dis_loss[index] = loss.data.cpu().numpy()[0]
+    self.dis_true_acc[index] = 0.5 * (true_a_acc + true_b_acc)
+    self.dis_fake_acc[index] = 0.5 * (fake_a_acc + fake_b_acc)
+    return loss
+
+  def both_gen_update(self, images_a, images_b, images_c, gan_w, vae_ll_w, vae_enc_w, quadro_w):
+    # a->b and a->c losses.
+    _, _, x_ab, _, gen_loss_b = self.gen_update(
+        0, images_a, images_b, gan_w, vae_ll_w, vae_enc_w)
+    _, _, x_ac, _, gen_loss_c = self.gen_update(
+        1, images_a, images_c, gan_w, vae_ll_w, vae_enc_w)
+
+    # a->b->d vs a->c->d loss.
+    x_bd, _ = self.gen[1].translate_a_to_b(x_ab, self.gpu)
+    x_cd, _ = self.gen[0].translate_a_to_b(x_ac, self.gpu)
+    ll_loss_d = torch.mean(torch.abs(x_bd - x_cd)) * x_bd.size(1) * x_bd.size(2) * x_bd.size(3)
+    #ll_loss_d = self._compute_ll_loss(x_bd, x_cd)
+    self.gen_ll_loss_d = ll_loss_d.data.cpu().numpy()[0]
+    
+    gen_loss = gen_loss_b + gen_loss_c + quadro_w * ll_loss_d
+    self.gen_total_loss = gen_loss.data.cpu().numpy()[0]
+    gen_loss.backward()
+    self.gen_opt.step()
+    return x_ab, x_ac, x_bd, x_cd
+
+  def both_dis_update(self, images_a1, images_b1, images_c1, images_a2, images_b2, images_c2):
+    dis_loss_b = self.dis_update(0, images_a1, images_b1, images_a2, images_b2)
+    dis_loss_c = self.dis_update(1, images_a1, images_c1, images_a2, images_c2)
+    dis_loss = dis_loss_b + dis_loss_c
+    dis_loss.backward()
+    self.dis_opt.step()
+
 
